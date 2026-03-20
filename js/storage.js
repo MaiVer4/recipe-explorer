9dfe34cebd3f86377afef13999f12fa75267be33
Vera <ll3925340@gmail.com>
2026-03-20 14:56:11 -0500

    feat: add new functions

--- js/storage.js
+++ js/storage.js
@@ -1,10 +1,20 @@
-const HISTORY_KEY = "recipe_search_history";
+const ITEMS_KEY = "items_per_page";
+const HISTORY_KEY = "search_history"; 
+
+export function saveItemsPerPage(value) {
+    localStorage.setItem(ITEMS_KEY, value);
+}
+
+export function getItemsPerPage() {
+    return Number(localStorage.getItem(ITEMS_KEY)) || 10;
+}
+
 
 export function saveSearchHistory(history) {
     localStorage.setItem(HISTORY_KEY, JSON.stringify(history));
 }
 
 export function getSearchHistory() {
-    const data = localStorage.getItem(HISTORY_KEY);
-    return data ? JSON.parse(data) : [];
+    const history = localStorage.getItem(HISTORY_KEY);
+    return history ? JSON.parse(history) : [];
 }
